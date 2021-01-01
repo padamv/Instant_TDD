415170e6c0b14867481be5529a0c2f9377d12aee
padamv <padamv91@gmail.com>
2021-01-01 16:38:09 +0100

    Pass empty string

--- wordcount/src/main/java/uk/co/ashleyfrieze/wordcount/WordCounter.java
+++ wordcount/src/main/java/uk/co/ashleyfrieze/wordcount/WordCounter.java
@@ -1,13 +1,15 @@
 package uk.co.ashleyfrieze.wordcount;
 
 public class WordCounter {
+	
+	private int wordsSoFar = 0;
 
 	public void add(String sentence) {
 		
 	}
 	
 	public int getUniqueWordsCount() {
-		return -1;
+		return wordsSoFar;
 	}
 
 }
