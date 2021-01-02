93ef8e5168cb94e4cfc513f088004cb310f5fbce
padamv <padamv91@gmail.com>
2021-01-02 13:35:43 +0100

    Refractor test with @Before and @After

--- before-after/src/test/java/co/uk/ashleyfrieze/testfolder/ReaderWriterTest.java
+++ before-after/src/test/java/co/uk/ashleyfrieze/testfolder/ReaderWriterTest.java
@@ -7,31 +7,42 @@ import java.io.File;
 import java.util.Arrays;
 import java.util.List;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class ReaderWriterTest {
+	private File writtenFile;
+	private File tempFolder;
+	
+	@Before
+	public void setup() throws Exception{
+		// with a temporary folder to write to
+		tempFolder = File.createTempFile("tmp", "file");
+		tempFolder.delete();
+		tempFolder.mkdir();
+	}
+	
+	@After
+	public void teardown() throws Exception{
+		
+		// and they tidy up in successful
+		assertTrue(writtenFile.delete());
+		assertTrue(tempFolder.delete());
+	}
 
 	@Test
 	public void readsLinesItWrote() throws Exception {
-		// with a temporary folder to write to
-		File tempFolder = File.createTempFile("tmp", "file");
-		tempFolder.delete();
-		tempFolder.mkdir();
-		
 		// when we write the file
 		List<String> someLines = Arrays.asList("One", "Two", "Three");
-		File writtenFile = ReaderWriter.write(tempFolder, "MyFile", someLines);
+		writtenFile = ReaderWriter.write(tempFolder, "MyFile", someLines);
 		
 		// then we can read it back
 		List<String> readLines = ReaderWriter.read(writtenFile);
 		
 		// and they match
 		assertEquals(someLines, readLines);
-		
-		// and they tidy up in successful
-		assertTrue(writtenFile.delete());
-		assertTrue(tempFolder.delete());
-	
+
 	}
 
 }
