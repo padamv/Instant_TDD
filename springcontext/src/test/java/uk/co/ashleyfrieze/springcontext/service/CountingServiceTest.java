f9a0f3c1e5e163d533e5c5786a62ba90f2a0d250
padamv <padamv91@gmail.com>
2021-01-03 11:22:16 +0100

    Add JUnit test

---
+++ springcontext/src/test/java/uk/co/ashleyfrieze/springcontext/service/CountingServiceTest.java
@@ -0,0 +1,14 @@
+package uk.co.ashleyfrieze.springcontext.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CountingServiceTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
