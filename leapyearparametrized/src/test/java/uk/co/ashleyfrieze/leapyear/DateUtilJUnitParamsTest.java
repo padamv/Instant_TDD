f6d41f51ae58227c5f1fd2b073a7b69c7588213f
padamv <padamv91@gmail.com>
2021-01-03 10:28:29 +0100

    JUnitParamsRunner first

---
+++ leapyearparametrized/src/test/java/uk/co/ashleyfrieze/leapyear/DateUtilJUnitParamsTest.java
@@ -0,0 +1,30 @@
+package uk.co.ashleyfrieze.leapyear;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import junitparams.naming.TestCaseName;
+
+@RunWith(JUnitParamsRunner.class)
+public class DateUtilJUnitParamsTest {
+	
+	@Test
+	@Parameters({
+		"1992, true",
+		"1996, true",
+		"1991, fales",
+		"1900, false",
+		"2000, true",
+	})
+	@TestCaseName("The year {0} should have is leap as {1}")
+	public void isLeapYear(int year, boolean shouldBeLeap) {
+		assertThat(DateUtil.isLeapYear(year), is(shouldBeLeap));
+	}
+	
+	
+}
