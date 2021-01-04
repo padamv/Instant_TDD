8c81d47a58f1b96b423227506feab1df61cca94a
padamv <padamv91@gmail.com>
2021-01-04 10:14:24 +0100

    Final form

--- spectrum/src/test/java/uk/co/ashleyfrieze/SpectrumTest.java
+++ spectrum/src/test/java/uk/co/ashleyfrieze/SpectrumTest.java
@@ -1,10 +1,11 @@
 package uk.co.ashleyfrieze;
 
-//import static org.junit.Assert.*;
+import static org.junit.Assert.*;
 import static com.greghaskins.spectrum.dsl.specification.Specification.describe;
 import static com.greghaskins.spectrum.dsl.specification.Specification.it;
 import static com.greghaskins.spectrum.dsl.specification.Specification.let;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.offset;
 
 import java.util.function.Supplier;
 
@@ -14,14 +15,28 @@ import org.junit.runner.RunWith;
 import com.greghaskins.spectrum.Spectrum;
 
 @RunWith(Spectrum.class)
-public class SpectrumTest {{
+public class SpectrumTest {
+	{
+		describe("Rectangles, eh!", () -> {
+			describe("A square", () -> {
+				Supplier<Rectangle> theSquare = let(() -> new Rectangle(5, 5));
+				it("is square", () -> {
+					assertThat(theSquare.get().isSquare()).isTrue();
+				});
+				it("has aspect ratio of 1", () -> {
+					assertThat(theSquare.get().getAspectRatio()).isCloseTo(1.0, offset(0.01));
+				});
+				it("has same sides", () -> {
+					assertThat(theSquare.get().getSides()).containsExactly(5, 5);
+				});
+			});
 
-	describe("A square", () -> {
-		Supplier<Rectangle> theSquare = let(() -> new Rectangle(5, 5));
-		it("is square", () -> {
-			assertThat(theSquare.get().isSquare()).isTrue();
+			describe("A rectangle", () -> {
+				it("is not square", () -> {
+					assertThat(new Rectangle(10, 2).isSquare()).isFalse();
+				});
+			});
 		});
-	});
 
-
-}}
+	}
+}
