a1d881513575c34854dc5ad5eb463d5570837429
padamv <padamv91@gmail.com>
2021-01-04 10:02:34 +0100

    With supplier

--- spectrum/src/test/java/uk/co/ashleyfrieze/SpectrumTest.java
+++ spectrum/src/test/java/uk/co/ashleyfrieze/SpectrumTest.java
@@ -1,10 +1,12 @@
 package uk.co.ashleyfrieze;
 
-import static org.junit.Assert.*;
+//import static org.junit.Assert.*;
 import static com.greghaskins.spectrum.dsl.specification.Specification.describe;
 import static com.greghaskins.spectrum.dsl.specification.Specification.it;
+import static com.greghaskins.spectrum.dsl.specification.Specification.let;
+import static org.assertj.core.api.Assertions.assertThat;
 
-
+import java.util.function.Supplier;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,8 +17,9 @@ import com.greghaskins.spectrum.Spectrum;
 public class SpectrumTest {{
 
 	describe("A square", () -> {
+		Supplier<Rectangle> theSquare = let(() -> new Rectangle(5, 5));
 		it("is square", () -> {
-			
+			assertThat(theSquare.get().isSquare()).isTrue();
 		});
 	});
 
