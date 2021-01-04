9d1cd859ce59ec9562e6b1d765676ddc35e4a95e
padamv <padamv91@gmail.com>
2021-01-04 09:59:23 +0100

    Starter state

---
+++ spectrum/src/test/java/uk/co/ashleyfrieze/SpectrumTest.java
@@ -0,0 +1,24 @@
+package uk.co.ashleyfrieze;
+
+import static org.junit.Assert.*;
+import static com.greghaskins.spectrum.dsl.specification.Specification.describe;
+import static com.greghaskins.spectrum.dsl.specification.Specification.it;
+
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.greghaskins.spectrum.Spectrum;
+
+@RunWith(Spectrum.class)
+public class SpectrumTest {{
+
+	describe("A square", () -> {
+		it("is square", () -> {
+			
+		});
+	});
+
+
+}}
